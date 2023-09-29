91330bde9bfeefb4bc48482e13aa9374966bed4e
Mustakim <m.m.kiron@gmail.com>
2023-09-29 18:26:12 +0600

    Add animals data in the table

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@
 CREATE TABLE animals (
     id SERIAL PRIMARY KEY,
     name varchar(100),
-    data_of_birth DATE,
+    date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL
