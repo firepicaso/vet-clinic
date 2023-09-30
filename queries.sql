13590d376edeb77ba4f94ad4a7f81bf25c89f74b
Mustakim <m.m.kiron@gmail.com>
2023-09-30 11:57:19 +0600

    Add queries

--- queries.sql
+++ queries.sql
@@ -33,3 +33,22 @@ UPDATE animals SET weight_kg = weight_kg * -1;
 ROLLBACK TO my_savepoint;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, SUM(escape_attempts) AS total_escapes
+FROM animals
+GROUP BY neutered
+ORDER BY total_escapes DESC
+LIMIT 1;
+
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
