8bbf1471dbe91ccf91929d97a1fc29d51e3832a9
Mustakim <m.m.kiron@gmail.com>
2023-09-30 17:13:02 +0600

    Add queries based on assignment

--- data.sql
+++ data.sql
@@ -13,3 +13,23 @@ VALUES
 ('Boarmon', '2005-06-07', 20.4, true, 7),
 ('Blossom', '1998-10-13', 17, true, 3),
 ('Ditto', '2022-05-14', 22, true, 4);
+
+INSERT INTO owners (full_name, age) VALUES
+('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
+
+INSERT INTO species (name) VALUES
+('Pokemon'),
+('Digimon');
+
+UPDATE animals
+SET species_id = (SELECT id FROM species WHERE name = 'Digimon')
+WHERE RIGHT(name, 3) = 'mon';
+
+UPDATE animals
+SET species_id = (SELECT id FROM species WHERE name = 'Pokemon')
+WHERE species_id IS NULL;
