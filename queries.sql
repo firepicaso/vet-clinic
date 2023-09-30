0409fd75a17e8b1c3d4173b7516dcd3a1611fdb4
Mustakim <m.m.kiron@gmail.com>
2023-09-30 16:44:37 +0600

    Create species and owner tables

--- queries.sql
+++ queries.sql
@@ -9,8 +9,6 @@ SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
-ALTER TABLE animals ADD COLUMN species VARCHAR(50);
-
 BEGIN;
 UPDATE animals SET species = 'unspecified';
 SELECT * FROM animals;
