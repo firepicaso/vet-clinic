dba6292b48f21c20d0499ce339b060323246efd8
Mustakim <m.m.kiron@gmail.com>
2023-09-30 13:09:23 +0600

    Added ALTER TABLE command and SLECT * FROM anilas to the queries

--- queries.sql
+++ queries.sql
@@ -9,22 +9,26 @@ SELECT * FROM animals WHERE neutered = TRUE;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+ALTER TABLE animals ADD COLUMN species VARCHAR(50);
+
 BEGIN;
 UPDATE animals SET species = 'unspecified';
 SELECT * FROM animals;
-SELECT * FROM animals;
 ROLLBACK;
+SELECT * FROM animals;
 
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 SELECT * FROM animals;
 COMMIT;
+SELECT * FROM animals;
 
 BEGIN;
 DELETE FROM animals;
 SELECT * FROM animals;
 ROLLBACK;
+SELECT * FROM animals;
 
 BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022-01-01';
@@ -33,6 +37,7 @@ UPDATE animals SET weight_kg = weight_kg * -1;
 ROLLBACK TO my_savepoint;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
+SELECT * FROM animals;
 
 SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
